6afdc8d6e60c2cfdc0fe2feeae229ef0fe177db8
Lyan <BEN9145@outlook.com>
2025-06-28 12:15:32 +0800

    jb;

--- sasu_icar2025_demo/src/include/thread.hpp
+++ sasu_icar2025_demo/src/include/thread.hpp
@@ -81,11 +81,11 @@ bool Factory<T>::consume(T &product) {
 	return true;
 }
 // ------------------------------------ //
-void signalHandler(int signal)
+void signalHandler(int signal);
 
-bool producer(Factory<TaskData> &task_data, Factory<TaskData> &AI_task_data, Capture &capture)
-bool AIConsumer(Factory<TaskData> &task_data, std::vector<PredictResult> &predict_result, Motion &motion)
-bool consumer(Factory<TaskData> &task_data, Factory<DebugData> &debug_data, std::vector<PredictResult> &predict_result, Motion &motion, Uart &uart)
+bool producer(Factory<TaskData> &task_data, Factory<TaskData> &AI_task_data, Capture &capture);
+bool AIConsumer(Factory<TaskData> &task_data, std::vector<PredictResult> &predict_result, Motion &motion);
+bool consumer(Factory<TaskData> &task_data, Factory<DebugData> &debug_data, std::vector<PredictResult> &predict_result, Motion &motion, Uart &uart);
 // void drawBox(Mat &img, std::vector<PredictResult> results);
 bool debugDataConsumer(Factory<DebugData> & debug_data);
 
