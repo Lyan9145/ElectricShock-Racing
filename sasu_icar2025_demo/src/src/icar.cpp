7972de1b59d6c5d7eb3df7a662c49b2a32c288f7
Lyan <BEN9145@outlook.com>
2025-06-30 15:46:07 +0800

    take2.6

--- sasu_icar2025_demo/src/src/icar.cpp
+++ sasu_icar2025_demo/src/src/icar.cpp
@@ -4,13 +4,13 @@
 #include <signal.h>
 #include <unistd.h>
 
-#include "../include/common.hpp"     // 公共类方法文件
-#include "../include/detection.hpp"  // 百度Paddle框架移动端部署
-#include "../include/uart.hpp"       // 串口通信驱动
-#include "../include/motion.hpp"     // 运动控制类
-#include "../include/thread.hpp"     // 线程管理
+#include "../include/common.hpp"        // 公共类方法文件
+#include "../include/detection.hpp"     // 百度Paddle框架移动端部署
+#include "../include/uart.hpp"          // 串口通信驱动
+#include "../include/motion.hpp"        // 运动控制类
+#include "../include/thread.hpp"        // 线程管理
 #include "../include/controlcenter.hpp" // 控制中心
-#include "../include/tracking.hpp"   // 跟踪算法
+#include "../include/tracking.hpp"      // 跟踪算法
 #include "../include/bridge.hpp"
 #include "../include/catering.hpp"
 #include "../include/crosswalk.hpp"
@@ -19,7 +19,6 @@
 #include "../include/parking.hpp"
 #include "../include/crossroad.hpp"
 #include "../include/ring.hpp"
-
 
 using namespace std;
 using namespace cv;
@@ -30,7 +29,6 @@ Display display; // 初始化UI显示窗口
 // 全局变量用于信号处理
 shared_ptr<Uart> g_uart = nullptr;
 volatile sig_atomic_t g_exit_flag = 0;
-
 
 // 图像信息显示函数
 void displayImageInfo(const Mat &img, long preTime)
@@ -51,11 +49,11 @@ void displayImageInfo(const Mat &img, long preTime)
   }
 }
 
-
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   // 初始化配置文件
   Motion motion;
-  
+
   // USB转串口初始化： /dev/ttyUSB0
   shared_ptr<Uart> uart = make_shared<Uart>("/dev/ttyUSB0"); // 初始化串口驱动
 
@@ -71,7 +69,7 @@ int main(int argc, char const *argv[]) {
     printf("[Error] Uart Open failed!\n");
     exit(-1);
   }
-  uart->startReceive(); // 启动数据接收子线程
+  uart->startReceive();             // 启动数据接收子线程
   uart->carControl(0, PWMSERVOMID); // 停车舵机中位
 
   // USB摄像头初始化
@@ -79,7 +77,8 @@ int main(int argc, char const *argv[]) {
   // 使用 std::to_string 将 int 转换为字符串，以确保兼容性
   std::string gstreamer_pipeline =
       "v4l2src device=/dev/video0 ! "
-      "image/jpeg,width=" + std::to_string(COLSIMAGE_CAM) +
+      "image/jpeg,width=" +
+      std::to_string(COLSIMAGE_CAM) +
       ",height=" + std::to_string(ROWSIMAGE_CAM) +
       ",framerate=60/1 ! " // 60/1 表示 60fps
       "jpegdec ! "         // 解码 MJPG 压缩流到原始格式 (x-raw)
@@ -89,61 +88,29 @@ int main(int argc, char const *argv[]) {
   // 使用 GStreamer 管道和 cv::CAP_GSTREAMER 标志初始化 VideoCapture
   cv::VideoCapture capture;
   capture = cv::VideoCapture(gstreamer_pipeline, cv::CAP_GSTREAMER);
-  if (!capture.isOpened()) {
+  if (!capture.isOpened())
+  {
     std::cerr << "Error: Could not open video capture with GStreamer pipeline." << std::endl;
     return -1;
   }
 
+  Factory<TaskData> task_factory(3);
+  Factory<TaskData> AI_task_factory(3);
+  Factory<DebugData> debug_factory(5);
+  std::vector<PredictResult> predict_result;
 
+  std::thread task_producer(&producer, std::ref(task_factory), std::ref(AI_task_factory), std::ref(capture));
+  std::thread AI_consumer(&AIConsumer, std::ref(task_factory), std::ref(predict_result), std::ref(motion));
+  std::thread task_consumer(&consumer, std::ref(task_factory), std::ref(debug_factory), std::ref(predict_result), std::ref(motion), std::ref(*uart));
+  // std::thread debug_data_consumer(&debugDataConsumer, std::ref(debug_factory));
+  // debug_data_consumer.join();
 
-	Factory<TaskData> task_factory(3);
-	Factory<TaskData> AI_task_factory(3);
-	Factory<DebugData> debug_factory(5);
-	std::vector<PredictResult> predict_result;
-	
-	std::thread task_producer(&producer, std::ref(task_factory), std::ref(AI_task_factory), std::ref(capture));
-	std::thread AI_consumer(&AIConsumer, std::ref(task_factory), std::ref(predict_result), std::ref(motion));
-	std::thread task_consumer(&consumer, std::ref(task_factory), std::ref(debug_factory), std::ref(predict_result), std::ref(motion), std::ref(*uart));
-		// std::thread debug_data_consumer(&debugDataConsumer, std::ref(debug_factory));
-		// debug_data_consumer.join();
-
-	
-	task_producer.join();
-	task_consumer.join();
-	AI_consumer.join();
-
-
+  task_producer.join();
+  task_consumer.join();
+  AI_consumer.join();
 
   uart->close(); // 串口通信关闭
   capture.release();
   return 0;
 }
 
-/**
- * @brief 鼠标的事件回调函数
- *
- */
-void mouseCallback(int event, int x, int y, int flags, void *userdata)
-{
-  double value;
-  switch (event)
-  {
-  case EVENT_MOUSEWHEEL: // 鼠标滑球
-  {
-    value = getMouseWheelDelta(flags); // 获取滑球滚动值
-    if (value > 0)
-      display.index++;
-    else if (value < 0)
-      display.index--;
-
-    if (display.index < 0)
-      display.index = 0;
-    if (display.index > display.frameMax)
-      if (display.index > display.frameMax)
-        display.index = display.frameMax;
-    break;
-  }
-  default:
-    break;
-  }
-}
