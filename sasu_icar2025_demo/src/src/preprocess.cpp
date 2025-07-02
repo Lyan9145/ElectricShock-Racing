d9c3bc226efa8b4ae95806957588e0e2f122e488
Lyan <BEN9145@outlook.com>
2025-07-02 19:10:00 +0800

    尝试矫正不裁切

--- sasu_icar2025_demo/src/src/preprocess.cpp
+++ sasu_icar2025_demo/src/src/preprocess.cpp
@@ -100,13 +100,15 @@ Mat Preprocess::correction(Mat &image)
 
     	// 1. Undistort the low-resolution image using the pre-calculated matrices.
 		cv::Mat dst;
-		cv::undistort(image, dst, PRECOMPUTED_MTX_LOW_RES, PRECOMPUTED_DIST, PRECOMPUTED_NEW_MTX_LOW_RES);      
+		cv::undistort(image, dst, PRECOMPUTED_MTX_LOW_RES, PRECOMPUTED_DIST, PRECOMPUTED_NEW_MTX_LOW_RES);   
+		return dst;   
 
 		// 2. Crop to the valid region using the pre-calculated ROI.
 		// .clone() creates a deep copy, making the returned Mat independent.   
-		cv::Mat cropped_dst = dst(PRECOMPUTED_ROI_LOW_RES).clone();
+		// cv::Mat cropped_dst = dst(PRECOMPUTED_ROI_LOW_RES).clone();
 
-		return cropped_dst;
+		// return cropped_dst;
+
 	}
 	else
 	{
