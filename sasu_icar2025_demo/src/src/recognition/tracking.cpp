118e416a58e9af8a45e6f3549bed68cfc5a098f5
Lyan <BEN9145@outlook.com>
2025-07-05 15:33:09 +0800

    take3.3

--- sasu_icar2025_demo/src/src/recognition/tracking.cpp
+++ sasu_icar2025_demo/src/src/recognition/tracking.cpp
@@ -1020,7 +1020,7 @@ void Tracking::trackRecognition_new(Mat &imageBinary, Mat &result_img, TaskData 
     int aim_angle_pwm = 0;
     aim_angle_pwm = (int)(pid_realize_a(aim_angle_filter, 0.0f, aim_angle_p, aim_angle_d) + 0.5f);
     cout << "> aim_angle: " << aim_angle_pwm << endl;
-    aim_angle_pwm = 5000 - clip(aim_angle_pwm, -1000, 1000);
+    aim_angle_pwm = 5000 + clip(aim_angle_pwm, -1000, 1000);
     src.steering_pwm = aim_angle_pwm;
 
 
