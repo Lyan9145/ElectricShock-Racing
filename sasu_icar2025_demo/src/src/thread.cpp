5418c5864e070462278a24981f1fd73e9eaeafa7
Lyan <BEN9145@outlook.com>
2025-07-12 11:42:31 +0800

    ？5

--- sasu_icar2025_demo/src/src/thread.cpp
+++ sasu_icar2025_demo/src/src/thread.cpp
@@ -218,8 +218,8 @@ bool producer(Factory<TaskData> &task_data, Factory<AIData> &AI_task_data, cv::V
 			src.timestamp = frameStartTime;
 			// 图像预处理
 			// src.img = img_buffer.clone(); // 克隆图像数据
-			displayImageInfo(img_buffer, preTime1, "capture");
 			resize(img_buffer, src.img, Size(640, 480), 0, 0, INTER_LINEAR);
+			displayImageInfo(src.img, preTime1, "capture");
 			src.img = preprocess.correction(img_buffer); // 图像矫正 
 			src.img = preprocess.resizeImage(src.img); // 图像尺寸标准化
 
